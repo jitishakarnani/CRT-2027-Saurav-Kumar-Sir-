14e7d1054b2a59fa7f52e021d5876dc2efa44953
jitishakarnani <jitisha278@gmail.com>
2026-05-06 14:54:24 +0530

    normal commit

---
+++ 14-E-CRT-/Day-6/constructors.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+class students
+{
+    // Access specifier
+public:
+
+    // Data members
+    string name;
+    int roll_no;
+
+    // Parameterized constructors
+    students(int r, string n){
+        roll_no=r;
+        name=n;
+    }
+
+    //Default constructors
+     students(){
+        roll_no=0;
+        name="";
+    }
+
+    // Member functions
+    void show()
+    {
+        cout << "Name:" << name << endl;
+        cout << "Roll No: " << roll_no << endl;
+    }
+};
+int main()
+{
+    students s1;
+    //students s1(303, "Janvi"); for parameterized constructors
+    s1.show();
+    return 0;
+}
