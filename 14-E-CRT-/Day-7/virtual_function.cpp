df9a759ebafbf6337b9655c7f9c00c8f7e67d01f
jitishakarnani <jitisha278@gmail.com>
2026-05-08 13:14:27 +0530

    normal commit

--- 14-E-CRT-/Day-7/virtual_function.cpp
+++ 14-E-CRT-/Day-7/virtual_function.cpp
@@ -16,40 +16,29 @@ public:
     {
         runs = r;
     }
+    virtual void show()
+    {
+        cout << "Virtaul Fun" << endl;
+        cout << "Name: " << name << endl;
+        cout << "Runs: " << runs << endl;
+    }
 };
 // Dervied Class or Child class
 class Cricketer : public Player
 {
 public:
     string country;
-    void setCountry(string s)
+    Cricketer(string s)
     {
         country = s;
     }
+
     void show()
     {
+        cout << "Child fun call" << endl;
         cout << "Name: " << name << endl;
         cout << "Runs: " << runs << endl;
         cout << "Country: " << country << endl;
-    }
-};
-
-class Footballer : public Player
-{
-public:
-    string club;
-    int goals;
-    void setClub(string c, int goals)
-    {
-        club = c;
-        this->goals = goals;
-    }
-
-    void show()
-    {
-        cout << "Name: " << name << endl;
-        cout << "Club: " << club << endl;
-        cout << "Goals: " << goals << endl;
     }
 };
 
@@ -57,15 +46,9 @@ int main()
 {
     int runs;
     cin >> runs;
-    Cricketer c1;
-    c1.setCountry("India");
+    Cricketer c1("India");
     c1.setName("Virat kohli");
     c1.setRuns(runs);
     c1.show();
-
-    Footballer f1;
-    f1.setName("Sunil Chhetri");
-    f1.setClub("Bengluru fc", 85);
-    f1.show();
     return 0;
 }
