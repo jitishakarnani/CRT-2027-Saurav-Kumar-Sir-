ffe91a2e1c3948e9945697e321a5c39a61e42464
jitishakarnani <jitisha278@gmail.com>
2026-05-09 15:13:48 +0530

    normal commit

---
+++ 14-E-CRT-/Day-9/sumof_array.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Sum{
+    public:
+    int sum(int arr[],int n){
+        int sum =0;
+        for(int i=0; i<n; i++){
+            sum += arr[i];
+        }
+        return sum;
+
+    }
+};
+
+int main(){
+    int arr[5]={1,2,3,4,5};
+    int n= sizeof(arr)/sizeof(arr[0]);
+    Sum s1;
+    int ans = s1.sum(arr,n);
+    cout<< ans <<endl;
+    return 0;
+
+}
