b9835bfdc2899b6d800cfe61355f0781d5034637
jitishakarnani <jitisha278@gmail.com>
2026-05-12 16:18:28 +0530

    commit

--- 14-E-CRT-/Day-12/recursion.cpp
+++ 14-E-CRT-/Day-12/recursion.cpp
@@ -14,7 +14,7 @@ int func(int x = 5) {
 
 // Factorial by recursion
 
-int fac(int n = 5) { 
+int fac(int n) { 
     if (n == 1 || n == 0) {         
         return 1;          
     }
@@ -22,8 +22,10 @@ int fac(int n = 5) {
     return n * fac(n - 1);
 }
 
-int main() {              
+int main() { 
+    int n;
+    cin>>n;             
     cout << "Sum: "       << func() << endl; 
-    cout << "Factorial: " << fac()  << endl; 
+    cout << "Factorial: " << fac(n) << endl; 
     return 0;
 }
